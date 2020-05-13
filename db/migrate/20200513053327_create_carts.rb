5946584edd71c9907fd7165da1103b08f7c0f68b
Joyce <nokjo86@hotmail.com>
2020-05-13 15:36:10 +1000

    added cart & cart items table

---
+++ db/migrate/20200513053327_create_carts.rb
@@ -0,0 +1,9 @@
+class CreateCarts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :carts do |t|
+      t.references :profile, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
