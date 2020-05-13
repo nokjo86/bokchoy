dd213f19ae28585d5ad9a7ff63d58ecb5e96d01f
Joyce <nokjo86@hotmail.com>
2020-05-13 23:06:26 +1000

    added total in cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -3,7 +3,7 @@ before_action :authenticate_user!
 before_action :set_profile, only: [:show]
 
   def show
-    @cart = @profile.cart.listings
+    @cart = @profile.cart.listings.order(:title)
   end
 
 end
