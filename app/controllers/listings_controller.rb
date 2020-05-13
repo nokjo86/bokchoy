117c9db4775a536157d1cff7e996b18f5b457e04
Joyce <nokjo86@hotmail.com>
2020-05-13 15:17:11 +1000

    included price in view and controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,6 @@
 class ListingsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_listing, only: [:show, :edit, :destroy]
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
   
   def index
   end
@@ -27,7 +27,7 @@ class ListingsController < ApplicationController
 
   def update
     if @listing.update(listing_params)
-      redirect_to listing_path(@profile.id)
+      redirect_to listing_path(@listing.id)
     else
       render :edit
     end
@@ -47,6 +47,5 @@ class ListingsController < ApplicationController
   def listing_params
   params.require(:listing).permit(:title, :description, :price, :picture, :delivery)
   end
-
 
 end
