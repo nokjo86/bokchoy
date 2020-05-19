3c1b5b630f84c13e4b8daf1fbf3ebb7ee2dacdfa
Joyce <nokjo86@hotmail.com>
2020-05-19 14:22:05 +1000

    added username login for devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,25 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:login]
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
   has_one :profile, dependent: :destroy
   after_create :init_profile
+  attr_writer :login
 
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+ 
   def init_profile
     profile = Profile.new(user_id: self.id)
     profile.save(validate: false)
